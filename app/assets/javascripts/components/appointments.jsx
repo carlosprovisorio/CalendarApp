28c6ffaa155c8bc8969dee25bcc65c83304e62c2
Carlos <carlosprovisorio@icloud.com>
2017-07-09 15:00:40 -0400

    add state to input form

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -2,16 +2,26 @@ var Appointments = React.createClass({
   getInitialState: function() {
     return {
       appointments: this.props.appointments,
-      input_title: '',
-      input_appt_time: 'Tomorrow at 9am'
+      title: 'Team standup meeting',
+      appt_time: 'Tomorrow at 9am'
     }
+  },
+
+  handleUserInput: function(obj) {
+    this.setState(obj);
   },
 
   render: function() {
     return (
       <div>
-        <AppointmentForm input_title={this.state.input_title} input_appt_time={this.state.input_appt_time} />
-        <AppointmentsList appointments={this.state.appointments} />
+        <AppointmentForm 
+          input_title={this.state.title} 
+          input_appt_time={this.state.appt_time} 
+          onUserInput={this.handleUserInput} 
+        />
+        <AppointmentsList 
+          appointments={this.state.appointments} 
+        />
       </div>
     )
   }
