d356c6dfff0b17b764706a6cc25140bb53d5353d
Carlos <carlosprovisorio@icloud.com>
2017-07-10 15:47:37 -0400

    convert components Appointment and appointments_lis to Stateless components

--- app/assets/javascripts/components/appointments_list.jsx
+++ app/assets/javascripts/components/appointments_list.jsx
@@ -1,13 +1,8 @@
-class AppointmentsList extends React.Component {
-  render () {
-    return (
-      <div className="container">
-        {this.props.appointments.map(function(appointment) {
-          return (
-            <Appointment appointment={appointment} key={appointment.id}/>
-          )
-        })}
-      </div>
-    )
-  }
-}
+const AppointmentsList = ({appointments}) => 
+  <div className="container">
+    {appointments.map(function(appointment) {
+      return (
+        <Appointment appointment={appointment} key={appointment.id}/>
+      )
+    })}
+  </div>
