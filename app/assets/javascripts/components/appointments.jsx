2724500d625516baf45e187690def6c272381659
Carlos <carlosprovisorio@icloud.com>
2017-07-10 09:39:14 -0400

    add Moment library to display dates and times in js

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -33,8 +33,8 @@ var Appointments = React.createClass({
     return (
       <div>
         <AppointmentForm 
-          input_title={this.state.title} 
-          input_appt_time={this.state.appt_time} 
+          title={this.state.title} 
+          appt_time={this.state.appt_time} 
           onUserInput={this.handleUserInput}
           onFormSubmit={this.handleFormSubmit}
         />
